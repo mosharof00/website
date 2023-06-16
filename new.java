605e4f561330e3ff1f127fc54e21c4a38c28916c
Md.Mosharof Khan <118794820+mosharof00@users.noreply.github.com>
2023-06-17 01:46:13 +0600

    Add files via upload

---
+++ new.java
@@ -0,0 +1,8 @@
+import javax.swing.plaf.synth.SynthRadioButtonMenuItemUI;
+
+class project{
+    public static void main(String[] args) {
+        System.out.println("hellow world");
+        System.out.println("this is my first programme in java");
+    } 
+}
